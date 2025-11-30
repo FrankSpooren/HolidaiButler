2007982543d01ae845a3356f0be4c812a350a2f9
Frank Spooren <frankspooren@hotmail.com>
2025-11-30 15:19:15 +0100

    Resolve merge conflict in .env.example

--- sales-pipeline-module/frontend/src/store/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Store Index - Export all stores
- */
-
-export { default as useAuthStore } from './authStore';
-export { default as useDealStore } from './dealStore';
-export { default as useNotificationStore } from './notificationStore';
-export { default as useUIStore } from './uiStore';
